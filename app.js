78290e7b7077bb2ac7b240b36620f438323301f6
Exyons <exyons007@gmail.com>
2023-05-14 22:25:16 +0530

    updated database url

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ const sessionSecrete = process.env.SESSION_SECRET || "NotSoNiceScectetIGuess";
 // If they know it then ok, otherwise tell them to change password, probably someone else accessed their account
 
 // Connecting MongoDB database
-// const dbUrl = process.env.DB_URL 
-const dbUrl = "mongodb://127.0.0.1:27017/BookSellingApp";
+const dbUrl = process.env.DB_URL 
+// const dbUrl = "mongodb://127.0.0.1:27017/BookSellingApp";
 mongoose.connect(dbUrl)
     .then(() => {
         console.log("DB Connected");
