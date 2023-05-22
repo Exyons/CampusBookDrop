65f719a3b56c7f24fa2fe5972560b20820f5db8b
Exyons <exyons007@gmail.com>
2023-05-22 23:25:30 +0530

    Forgot to uglify reset password client js file and change id of new password

--- public/backup/js/reset_password.js
+++ public/backup/js/reset_password.js
@@ -18,7 +18,7 @@ resetPasswordBtn.addEventListener("click", async () => {
 })
 
 const checkPassword = () => {
-    const password = document.querySelector("#InputPassword");
+    const password = document.querySelector("#newPasswordVisibilityBtn");
     password.addEventListener("input", async () => {
         try {
             const res = await axios.post("/user/reset_password/checkPassword", { password: password.value })
