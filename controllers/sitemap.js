74d434f5147c5cf9377cfbd6e943235c501549e1
Exyons <exyons007@gmail.com>
2023-06-09 10:28:29 +0530

    modified path for sendfile in sitemap controller

--- controllers/sitemap.js
+++ controllers/sitemap.js
@@ -1,5 +1,7 @@
+const path = require("path")
+const root = __dirname
 const sendSitemap = (req, res) => {
-    res.sendFile("../sitemap/sitemap.xml")
+    res.sendFile(path.join(root, "../sitemap/sitemap.xml"))
 }
 
 module.exports = sendSitemap
